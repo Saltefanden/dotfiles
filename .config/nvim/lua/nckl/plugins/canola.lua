626296bd3b93dedf4be1f279b16310e73755c7c4
Saltefanden <nicklas.m.larsen@gmail.com>
2026-06-18 11:16:28 +0200

    Remove canola (immature library for ssh)

--- .config/nvim/lua/nckl/plugins/canola.lua
+++ .config/nvim/lua/nckl/plugins/canola.lua
@@ -1,10 +1,10 @@
-vim.pack.add({
-    'https://github.com/barrettruth/canola.nvim'
-})
-require('oil').setup(
-    {
-        -- default_file_explorer = false
-    }
-)
-
-vim.keymap.set('n', '<leader>1', "<Cmd>Oil<CR>", { desc = "Oil"})
+-- vim.pack.add({
+--     'https://github.com/barrettruth/canola.nvim'
+-- })
+-- require('oil').setup(
+--     {
+--         -- default_file_explorer = false
+--     }
+-- )
+--
+-- vim.keymap.set('n', '<leader>1', "<Cmd>Oil<CR>", { desc = "Oil"})
